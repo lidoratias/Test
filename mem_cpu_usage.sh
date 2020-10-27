10b835b2555f1365204d95d465fe3b50620924e7
lidoratias <lidor1atias@gmail.com>
2020-10-27 17:45:03 +0200

    updated - now appending to the file each hour instead of overrriding the current text.

--- mem_cpu_usage.sh
+++ mem_cpu_usage.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-if test -f "/tmp/hourly_log.txt"; then
-	rm /tmp/hourly_log.txt
+mkdir -p temp
+cd temp
+if [ ! -f hourly_log.txt ]
+then
+	touch hourly_log.txt
 fi
-top -b -n 1 > /tmp/hourly_log.txt
+top -b -n 1 >> hourly_log.txt
 echo "init hourly log with mem usage"
-mpstat >> /tmp/hourly_log.txt
+mpstat >> hourly_log.txt
 echo "finish"
